fcd38d4239a79db417a8d1778ee6791c85483c95
grainednoise <grainednoise@gmail.com>
2012-01-30 22:45:06 +0100

    PEP8 fixes

--- src/dirmetadata/blockreaderutils.py
+++ src/dirmetadata/blockreaderutils.py
@@ -1,5 +1,6 @@
 from greenlet import greenlet
 
+
 """
 This module contains a few ways to transform the block-push paradigm used by the DirMetaDataProvider into more familiar ones.
 """
@@ -13,73 +14,73 @@ def _general_push_pull_adaptor(func, coroutine_pull_builder):
     a nonempty block of data, and once with None, or an empty block of data.
 
     """
-    
+
     grnlt_access = [False]    # Ugliness as Python 2.x lacks 'nonlocal'
-    
+
     def reader(block):
         grnlt = grnlt_access[0]
-        
+
         if grnlt is None:
             return False
-        
+
         if grnlt is False:
             grnlt = grnlt_access[0] = greenlet(func)
             grnlt.switch(coroutine_pull_builder())
-            
+
             if grnlt.dead:
                 grnlt_access[0] = None
                 return False
-        
-               
+
+
         grnlt.switch(block)
-        
+
         if grnlt.dead:
             grnlt_access[0] = None
             return False
-        
+
         # After feeding an empty block, the greenlet should finish processing, and return (i.e: become dead).
         # If this does not occur, there is a serious flaw in the greenlet switching logic.
         assert block, "At this point, we must have a nonempty block"
-        
+
         return True
-    
-    
+
+
     return reader
 
 
 def _greenlet_block_generator():
     block = greenlet.getcurrent().parent.switch()
-    
+
     try:
         while block:
             yield block
             block = greenlet.getcurrent().parent.switch()
-    
+
     finally:
         block = None
 
 
 def reader_from_generator_accepting_function(func):
-    """Adapts a function which accepts a generator which yields blocks of data 
+    """Adapts a function which accepts a generator which yields blocks of data
     to a reader. A reader is a function which can be called multiple times with
     a nonempty block of data, and once with None, or an empty block of data.
 
     """
-    
+
     return _general_push_pull_adaptor(func, _greenlet_block_generator)
 
 
 
 def _greenlet_byte_generator():
     block = greenlet.getcurrent().parent.switch()
-    
+
     try:
         while block:
             for byte in block:
                 yield byte
-                
+
             block = greenlet.getcurrent().parent.switch()
-    
+
     finally:
         block = None
 
@@ -90,7 +91,7 @@ def reader_from_byte_generator_accepting_function(func):
     a nonempty block of data, and once with None, or an empty block of data.
 
     """
-    
+
     return _general_push_pull_adaptor(func, _greenlet_byte_generator)
 
 
@@ -98,8 +99,8 @@ def block_to_byte_generator_wrapper(block_generator):
     try:
         for block in block_generator:
             for byte in block:
-                yield byte 
-                
+                yield byte
+
     except GeneratorExit:
         block_generator.close()
 
@@ -109,110 +110,110 @@ def block_to_byte_generator_wrapper(block_generator):
 
 
 class _FileLikeBLockReader(object):
-    
+
     def __init__(self):
         self._closed = False
         self._current_block = None
         self._next_byte_index = None
         self._sum_previous_blocksizes = 0
-    
-        
+
+
     def read(self, requested):
         """ Reads *exactly* 'requested' bytes from the stream,
         unless there are not enough bytes in the stream. After
         the stream is exhausted, the stream will close automatically.
         After a stream is closed, only '' (empty strings)
-        will be returned. 
-        
-        @param requested: The number of requested bytes. Must be > 0. 
+        will be returned.
+
+        @param requested: The number of requested bytes. Must be > 0.
         """
-        
+
         assert requested > 0
 
         if self._closed:
             return ''
-        
+
         if self._current_block is None:
             self._read_next_block()
-            
+
             if self._closed:
                 return ''
-        
+
         elif self._next_byte_index >= len(self._current_block):
             self._read_next_block()
-            
+
             if self._closed:
                 return ''
-        
-        
+
+
         if self._next_byte_index + requested < len(self._current_block):
             new_index = self._next_byte_index + requested
             result = self._current_block[self._next_byte_index:new_index]
             self._next_byte_index = new_index
             return result
-        
-        
+
+
         block = self._current_block[self._next_byte_index:]
         requested -= len(block)
-        
+
         result = [block]
         while True:
             self._read_next_block()
-            
+
             if self._closed:
                 break
-            
+
             if len(self._current_block) >= requested:
                 self._next_byte_index = requested
                 result.append(self._current_block[:requested])
                 break
-            
+
             requested -= len(self._current_block)
             result.append(self._current_block)
-        
-        
+
+
         return ''.join(result)
-    
-    
-    
+
+
+
     def close(self):
         self._closed = True
         self._current_block = None
-    
-    
+
+
     @property
     def closed(self):
         return self._closed
-    
-    
+
+
     def __del__(self):
         self.close()
-    
-    
+
+
     def tell(self):
         return self._sum_previous_blocksizes + self._next_byte_index
-    
-    
+
+
     def seek(self, offset, whence=None):
         raise NotImplementedError()
-        
-    
+
+
     def _read_next_block(self):
         if self._closed:
             raise EOFError()
-            
+
         previous_block_size = len(self._current_block) if self._current_block is not None else 0
         self._sum_previous_blocksizes += previous_block_size
-        
+
         block = greenlet.getcurrent().parent.switch()
         if block:
             self._current_block = block
             self._next_byte_index = 0
-            
+
         else:
             self.close()
-    
-            
+
+
 
 class StreamReaderException(Exception):
     pass
@@ -227,5 +228,5 @@ def reader_from_file_like_object_accepting_function(func):
     A reader is a function which can be called multiple times with
     a nonempty block of data, and once with None, or an empty block of data.
     """
-    
+
     return _general_push_pull_adaptor(func, _FileLikeBLockReader)
--- src/dirmetadata/main.py
+++ src/dirmetadata/main.py
@@ -12,48 +12,48 @@ def _collect_plugin_paths(path):
 def _may_contain_valid_plugin_path(path):
     if not isdir(path):
         return False
-    
+
     return bool(_collect_plugin_paths(path))
 
 
 
 try:
-    _valid_provider_id_types = (str, unicode) #@UndefinedVariable
+    _valid_provider_id_types = (str, unicode)  # @UndefinedVariable
 
 except:
-    _valid_provider_id_types = (str, bytes) #@UndefinedVariable
-    
+    _valid_provider_id_types = (str, bytes)  # @UndefinedVariable
+
 
 
 def register_provider(clss):
     """This function should be called during import of a plugin to register
      a subclass of DirMetaDataProvider. It may be called only once for any
-     particular class, and the clss.category_name must be globally unique. 
+     particular class, and the clss.category_name must be globally unique.
     """
     assert isinstance(clss.category_name, _valid_provider_id_types)
     assert clss.category_name not in dirmetadata_providers
     dirmetadata_providers[clss.category_name] = clss
-    
+
 
 
 
 def enable_plugin_paths(candidate_paths):
     plugin_paths = tuple(filter(_may_contain_valid_plugin_path, candidate_paths))
-    
+
     for path in plugin_paths:
         if path not in sys.path:
             sys.path.append(path)
-    
+
     plugin_names = []
     for path in plugin_paths:
         plugin_names += _collect_plugin_paths(path)
-    
-    
+
+
     for plugin_name in plugin_names:
         __import__(plugin_name)
 
     print(plugin_names)
-    
+
 
 
 
@@ -61,50 +61,47 @@ class DirMetaDataProvider(object):
     """Base class for all metadata plugin classes.
     All derived classes must supply a unique category name.
     """
-    
+
     category_name = None
-    
-    
+
+
     def __init__(self, previously_stored_data=None):
         """
         @param previously_stored_data: a data structure containing serializable data
             as returned from the call to self.data() a previous occasion. Should be used
             when not all data can be parsed from the file (i.e. is input by the user) or
-            when historical data should be kept (versioning). 
+            when historical data should be kept (versioning).
         """
-    
+
 
     def data_generator(self, first_block_of_data, first_block_is_entire_file=False):
         """Must return a list of readers (which may be empty).
-        
+
         @param first_block_of_data: to determine whether or not you have readers for this type of data.
             The size of the block will be at least 64K, assuming of course the file is that long.
             There is be no set upper limit.
-        
+
         @param first_block_is_entire_file: when True, ``first_block_of_data`` is guaranteed to  contain
             all data in the file, and the entire processing may be done in this method. In that case,
             ``[]`` should be returned.
-            Please note that this is an optimization, and there may be circumstances in 
+            Please note that this is an optimization, and there may be circumstances in
             which this parameter is not set to True when the entire contents of the file actually
             *is* in ``first_block_of_data``.
-            
+
         @return: a list of readers
             A reader is a callable which can be called multiple times with as its sole parameter a block
             of binary data from the file. All blocks will have a size >= 1, except the last one, which
             will have a size of 0. Please make sure you do *not* store any references to the data blocks
             as they will generally be as large as practical, and having more than one of these blocks may
             cause the program to run out of memory.
-         
+
         """
         return []
 
-    
+
     def data(self):
         """
         Will only be called after the data_generator() is called, and all readers returned will
         have been fed all the file data.
         """
         raise NotImplementedError()
-
-
-
