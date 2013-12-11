e0bafcdcd77d775f92dad39ca2c6fc7887c474a0
grainednoise <grainednoise@gmail.com>
2013-12-11 16:22:03 +0100

    Fixed test, even though it's not all that useful

--- test/test_readers/readers.py
+++ test/test_readers/readers.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from dirmetadata.readers import FileSystemDirectoryReader
 import datetime
 import os.path
-from functools import wraps
+
+from dirmetadata.readers import FileSystemDirectoryReader
 
 
 test_directory = os.path.abspath(os.path.join(__file__, '..', '..', 'testdata'))
@@ -17,41 +17,15 @@ def test_not_empty_files():
     assert expected == set(reader().files())
 
 
-
 def test_attributes():
     result = reader().get_file_info('Lyrical Ballads 1798.txt')
     print result
     assert 151549 == result['size']
     assert 0o100666 == result['mode']
 
-    # 8 January 2012 12:27:56
-    assert datetime.datetime(2012, 1, 8, 12, 27, 56) == datetime.datetime.fromtimestamp(result['created']).replace(microsecond=0)
+    # 2012-09-16 09:10:17.168216000 +0200 Lyrical Ballads 1798.txt
+    assert datetime.datetime(2012, 9, 16, 9, 10, 17) == datetime.datetime.fromtimestamp(result['created']).replace(microsecond=0)
 
-    # ‎26 ‎May ‎2012, ‏‎10:42:27
+    # ‎2012-05-26 10:42:27.471178200 +0200 Lyrical Ballads 1798.txt
     assert datetime.datetime(2012, 5, 26, 10, 42, 27) == datetime.datetime.fromtimestamp(result['modified']).replace(microsecond=0)
-
-
-
-def pytest_generate_tests(metafunc):
-    if "topdesk_version" in metafunc.funcargnames:
-        metafunc.parametrize("topdesk_version", ["4.4sp2", "5.0"])
-
-
-
-
-def xxx(func):
-    @wraps(func)
-    def rv(topdesk_version):
-        func(topdesk_version)
-
-    return rv
-
-@xxx
-def test_versions(topdesk_version):
-    print topdesk_version
-    assert topdesk_version < "5"
-if __name__ == "__main__":
-
-    import nose
-    nose.runmodule()
 
